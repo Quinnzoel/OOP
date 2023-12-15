ccae35b1dc873a9bc1d771ebe68f8ceac6bf62fa
quinnzoel <quinnzorevn@gmail.com>
2023-12-15 22:07:21 +0700

    add sql

---
+++ JAVA.sql
@@ -0,0 +1,42 @@
+﻿create database JAVAOOP
+use JAVAOOP
+
+
+
+create table account 
+(
+username varchar(30) primary key,
+pass varchar(20) not null,
+ChucVu varchar(20) not null
+)
+
+create table cars 
+(
+CARID varchar(30) primary key,
+BRAND varchar(20) not null,
+MODEL varchar(20) not null,
+car_status varchar(20) not null,
+PRICE int not null
+)
+
+create table customer
+{
+CUSID varchar(30) primary key,
+HoTen varchar(20) not null,
+DiaChi varchar(20) not null,
+SoDienThoai varchar(20) not null
+}
+
+create table car_rented
+{
+CARID varchar(30),
+CUSID varchar(30),
+RENTID varchar(30),
+RENTDATE date not null,
+RETURNDATE date not null
+}
+
+
+
+drop table account
+select * from account
